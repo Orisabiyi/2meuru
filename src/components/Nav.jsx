432b1ab727f74c18efeb695fe4b4e7e68cbd3d82
Orisabiyi <orisabiyidavid@gmail.com>
2024-07-04 15:32:17 +0100

    wrote the jsx structure for nav

---
+++ src/components/Nav.jsx
@@ -0,0 +1,15 @@
+function Nav() {
+  return (
+    <nav>
+      <h1>2meuru</h1>
+      <ul>
+        <li>Home</li>
+        <li>Features</li>
+        <li>Benefits</li>
+      </ul>
+      <button>Download App</button>
+    </nav>
+  );
+}
+
+export default Nav;
