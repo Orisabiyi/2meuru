f6fa372399584879bcd71b0faebda17db8265908
Orisabiyi <orisabiyidavid@gmail.com>
2024-07-04 16:50:40 +0100

    wrote the jsx for header and included it in Homepage

---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+function Header() {
+  return (
+    <header>
+      <div>
+        <h1>
+          <span>Empower Parents</span>
+          <span>and Elevate Student Success</span>
+        </h1>
+        <p>
+          Track Performance | Monitor Activities | Manage Payments Effortlessly
+        </p>
+      </div>
+      <div></div>
+    </header>
+  );
+}
+
+export default Header;
