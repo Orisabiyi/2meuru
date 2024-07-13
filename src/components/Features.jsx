51c68bd5e5827cf01b99fc52b628965eb68cab73
Orisabiyi <orisabiyidavid@gmail.com>
2024-07-13 14:37:49 +0100

    section features jsx

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,5 +1,38 @@
+import styles from "./Features.module.css";
+
 function Features() {
-  return <section></section>;
+  return (
+    <section className={styles.feature}>
+      <figure>
+        <div>
+          <img src="" alt="" />
+        </div>
+        <h3>Track Student Scores</h3>
+        <p>
+          Parents can access detailed reports of their wards&apos; academi
+          performance
+        </p>
+      </figure>
+
+      <figure>
+        <div>
+          <img src="" alt="" />
+        </div>
+        <h3>Monitor Activities</h3>
+        <p>
+          Stay updated with your child&apos;s classroom activities and progress
+        </p>
+      </figure>
+
+      <figure>
+        <div>
+          <img src="" alt="" />
+        </div>
+        <h3>Manage Payment</h3>
+        <p>Simplify school fee payments and keep track of all transactions</p>
+      </figure>
+    </section>
+  );
 }
 
 export default Features;
