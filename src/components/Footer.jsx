fee3bb1d00bbe8c0b6f89f7fcf1c58c25151b1a5
Orisabiyi <orisabiyidavid@gmail.com>
2024-07-15 12:30:44 +0100

    styled the footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,6 +7,12 @@ function Footer() {
       <NavLink to="/">
         <h1>2meuru</h1>
       </NavLink>
+
+      <button>
+        <a href="https://drive.google.com/file/d/103YfeYZzqM1wEgzX8cFRGan7iEl1Au71/view?usp=drivesdk">
+          Download App
+        </a>
+      </button>
     </footer>
   );
 }
